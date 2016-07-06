039ac661c6daf6abb2faa0d09e3338e055f1b22c
Gina <gina.chernoby@outlook.com>
2016-07-06 14:03:06 -0700

    Initial commit

---
+++ OneDriveApiBrowser/Program.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OneDriveApiBrowser
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormBrowser());
+        }
+    }
+}
